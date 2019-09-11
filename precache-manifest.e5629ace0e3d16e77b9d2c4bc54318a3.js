c9fbc0c98e07866b240a42c60be417e02e4cbe01
Gabriel Domínguez Durán <gabrieldominguezduran@gmail.com>
2019-09-11 22:01:39 +0200

    Updates

---
+++ precache-manifest.e5629ace0e3d16e77b9d2c4bc54318a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0881555065f1b652d21c83640d66013a",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "3657a41fd8ab54a21a8b",
+    "url": "/MovieDB/static/css/main.990e593d.chunk.css"
+  },
+  {
+    "revision": "1d2dbde881f8b5df89af",
+    "url": "/MovieDB/static/js/2.1be053b8.chunk.js"
+  },
+  {
+    "revision": "3657a41fd8ab54a21a8b",
+    "url": "/MovieDB/static/js/main.957db9c4.chunk.js"
+  },
+  {
+    "revision": "382918c010c6efa4c4e1",
+    "url": "/MovieDB/static/js/runtime~main.2d8d63db.js"
+  }
+]);
